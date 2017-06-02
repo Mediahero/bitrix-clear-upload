89a40406cf81150dffce2d2051ef19a897fdfd0d
George <mail@gmelikov.ru>
2017-06-02 15:45:12 +0300

    Добавлена возможность перемещения файлов
    Signed-off-by: George <mail@gmelikov.ru>

--- clear_upload.php
+++ clear_upload.php
@@ -4,18 +4,27 @@
 if (count($argv) < 2) {
     echo <<< USAGE
 
-Usage: php clear_upload.php [--delete-files] /path/to/document/root
+Usage: php clear_upload.php [--delete-files] [--move-files=/path/to] /path/to/document/root
 
 Скрипт для очистки каталога upload/iblock от неиспользуемых файлов (оставшихся после удаления элемента инфоблока).
 Проверяет каждый файл в каталоге upload/iblock, есть ли он в таблице b_file и если его там нет выводит полный 
-путь к нему на экран, а если указана опция --delete-files, то удаляет файл. В режиме удаления (с опцией --delete-files), 
+путь к нему на экран.
+
+Если указана опция --move-file=/путь, то перемещает файл в указанную директорию с сохранением иерархии.
+
+Если указана опция --delete-files, то удаляет файл. В режиме удаления (с опцией --delete-files), 
 если каталог, в котором находился удаляемый файл становится пустым - удаляет и его.
+
 
 Примеры использования:
 
 Получить список всех неиспользуемых файлов из каталога upload/iblock:
     
     php clear_upload.php /var/www/example.com
+    
+Переместить все неиспользуемые файлы из каталога upload/iblock в папку /backup:
+    
+    php clear_upload.php --move-files=/backup /var/www/example.com
 
 Удалить все неиспользуемые файлы из каталога upload/iblock:
     
@@ -27,7 +36,19 @@ USAGE;
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-$deleteFiles = (count($argv) > 1 && $argv[1] == '--delete-files');
+$command='';
+$commandvalue='';
+
+// Проверяем аргументы
+if ( substr( $argv[1], 0, 2 ) === '--' ) {
+	$argv[1] = substr( $argv[1], 2 );
+	if (strpos($argv[1],'=')) {
+		list($command,$commandvalue) = explode("=",$argv[1],2);
+	} else {
+		$command = $argv[1];
+	}
+}
+
 $_SERVER['DOCUMENT_ROOT'] = $DOCUMENT_ROOT = count($argv) > 2 ? $argv[2] : $argv[1];
 
 #define("LANG", "ru"); 
@@ -61,18 +82,23 @@ while (false !== ($subDirName = readdir($hRootDir))) {
             continue;
         }
         $fullPath = "$subDirPath/$fileName";
-        if ($deleteFiles) {
+        if ($command=='delete-files') {
             if (unlink($fullPath)) {
                 echo "Removed: " . $fullPath . PHP_EOL;
             }
-        }
+        } elseif ($command=='move-files' && !empty($commandvalue)) {
+			mkdir("$commandvalue/$subDirName",0775, true);
+			if (rename($fullPath, "$commandvalue/$subDirName/$fileName")) {
+				echo "Move: " . $fullPath . PHP_EOL;
+			}
+		}
         else {
             $filesCount++;
             echo $fullPath . PHP_EOL;
         }
     }
     closedir($hSubDir);
-    if ($deleteFiles && !$filesCount) {
+    if ($command=='delete-files' && !$filesCount) {
         rmdir($subDirPath);
     }
 }
